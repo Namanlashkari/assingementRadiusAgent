01a0c5e2798957d737c02eca776c9cf84011b5f1
Deathshot_7496 <naman.lashkari@gmail.com>
2020-02-04 02:12:41 +0530

    Update SqlQuery.py

--- Solution1/SqlQuery.py
+++ Solution1/SqlQuery.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
-
+ 
 try:
     conn = mysql.connector.connect(host='localhost',
                                          database='realestate',
@@ -8,7 +8,7 @@ try:
                                          password='**********',
                                          auth_plugin='mysql_native_password')
 
-    '''
+   
     if conn.is_connected():
         db_Info = conn.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
@@ -16,24 +16,29 @@ try:
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
-    '''
-    '''
-    mysqlmySql_Create_Table_Query = """CREATE TABLE  properties( 
+   
+    mysqlmySql_Create_Table_Query = CREATE TABLE  properties( 
                                      Id int(11) NOT NULL unique ,
                                      latitude float NOT NULL,
                                      longitute float NOT NULL,
                                      Price float NOT NULL,
                                      bedroom int NOT NULL,
                                      bathroom int NOT NULL,
-                                     PRIMARY KEY (Id)) """
-
+                                     PRIMARY KEY (Id))
+    
+    mySql_Retrieve_Table_Query = Select * from properties p where (p.lat between Lat-0.145 and Lat+0.145) and (p.long between Long-0.182 and Long+0.182)
+    
+    
+    
     cursor = conn.cursor()
-    result = cursor.execute(mySql_Create_Table_Query)
-    print("Table created successfully ")
+    result = cursor.execute(mySql_Retrieve_Table_Query)
+    print("Data Retrieved")
+    
+    
     
 except mysql.connector.Error as error:
     print("Failed to create table in MySQL: {}".format(error))
-    '''
+    
 except Error as e:
     print("Error while connecting to MySQL", e)
 
